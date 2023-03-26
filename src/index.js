bb6e35b531c49a66d81ad1cd67c17978a9bf96db
Valentine <kerubovalentine1@gmail.com>
2023-03-26 06:14:24 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
+/*document.addEventListener("DOMContentLoaded", () => {
   // your code here
+});
+*/
+
+// Define global variables
+const form = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#tasks');
+
+// Listen for submit event on form
+form.addEventListener('submit', function(event) {
+  // Prevent default form behavior
+  event.preventDefault();
+  // Get input value
+  const input = document.querySelector('#new-task-description');
+  const inputValue = input.value;
+  // Create new task list item
+  const taskItem = document.createElement('li');
+  taskItem.innerText = inputValue;
+  // Add task to list
+  taskList.appendChild(taskItem);
+  // Reset input value
+  input.value = '';
+});
+
+// Stretch Deliverable: Delete function
+taskList.addEventListener('click', function(event) {
+  if (event.target.tagName === 'LI') {
+    event.target.remove();
+  }
+
+  
 });
